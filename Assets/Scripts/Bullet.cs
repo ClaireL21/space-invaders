842b38f6f1bc22fdd73c73b4bbd560276b7cbca6
ClaireL21 <53claire@gmail.com>
2024-09-09 20:11:45 -0400

    end of Part C basic gameplay

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -10,7 +10,7 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         // travel straight in the Z-axis
-        distance.z = -0.2f;
+        distance.z = -0.2f; 
     }
 
     // Update is called once per frame
